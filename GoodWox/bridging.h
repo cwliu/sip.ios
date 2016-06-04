1fd132c3b9dba59b7729b7cf7cdcadbdd3ccab70
Cody Liu <liu@codylab.com>
2016-06-04 17:38:45 +0800

    Add bridging.h

---
+++ GoodWox/bridging.h
@@ -0,0 +1,17 @@
+//
+//  bridging.h
+//  GoodWox
+//
+//  Created by Cody Liu on 6/4/16.
+//  Copyright © 2016 WiAdvance. All rights reserved.
+//
+
+#ifndef bridging_h
+#define bridging_h
+
+
+#endif /* bridging_h */
+
+#import <ADALiOS/ADAL.h>
+#import <orc/orc.h>
+#import <outlook_services/outlook_services.h>
