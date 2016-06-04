b23bc1d9cb9d18db608cb529e74d23ba20dfe966
Cody Liu <liu@codylab.com>
2016-06-04 17:46:11 +0800

    Add a login button

--- GoodWox/ViewController.swift
+++ GoodWox/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func login(){
+        NSLog("Login button clicked")
+    }
 }
 
