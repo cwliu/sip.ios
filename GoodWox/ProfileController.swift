c88f20a1326a1b9a14b5e6254ba70981b3045fc5
Cody Liu <liu@codylab.com>
2016-06-05 09:51:40 +0800

    Add empty ProfileController and a manual segue

---
+++ GoodWox/ProfileController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileController.swift
+//  GoodWox
+//
+//  Created by Cody Liu on 6/5/16.
+//  Copyright © 2016 WiAdvance. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProfileController: UIViewController{
+    
+
+}
