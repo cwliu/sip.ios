31484960d963e2165140a809ea945f37dd19509a
Cody Liu <liu@codylab.com>
2016-06-06 22:35:31 +0800

    Display dummy row

--- GoodWox/CompanyController.swift
+++ GoodWox/CompanyController.swift
@@ -10,4 +10,23 @@ import Foundation
 
 class CompanyController: UITableViewController{
     
+    override func viewDidLoad() {
+        //         Get the height of the status bar
+        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+        
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .Value1, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "Hello World"
+        return cell
+    }
 }
