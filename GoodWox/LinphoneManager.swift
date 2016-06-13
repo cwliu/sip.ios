eac948a173404d6db3d6f83bfcb13c98c46a430f
Cody Liu <liu@codylab.com>
2016-06-13 10:56:32 +0800

    Add a timer to iterate linphone core

--- GoodWox/LinphoneManager.swift
+++ GoodWox/LinphoneManager.swift
@@ -1,8 +1,5 @@
 import Foundation
 
-var answerCall: Bool = false
-
-
 var registrationStateChanged: LinphoneCoreRegistrationStateChangedCb = {
     (lc: COpaquePointer, proxyConfig: COpaquePointer, state: LinphoneRegistrationState, message: UnsafePointer<Int8>) in
     
@@ -34,11 +31,6 @@ var callStateChanged: LinphoneCoreCallStateChangedCb = {
     case LinphoneCallIncomingReceived: /**<This is a new incoming call */
         NSLog("callStateChanged: LinphoneCallIncomingReceived")
         
-        if answerCall{
-            ms_usleep(3 * 1000 * 1000); // Wait 3 seconds to pickup
-            linphone_core_accept_call(lc, call)
-        }
-        
     case LinphoneCallStreamsRunning: /**<The media streams are established and running*/
         NSLog("callStateChanged: LinphoneCallStreamsRunning")
         
@@ -52,7 +44,9 @@ var callStateChanged: LinphoneCoreCallStateChangedCb = {
 
 class LinphoneManager {
     
-    var lc: COpaquePointer!
+    static var lc: COpaquePointer!
+    static var iterateTimer: NSTimer!
+    
     var lct: LinphoneCoreVTable = LinphoneCoreVTable()
     
     init() {
@@ -73,14 +67,41 @@ class LinphoneManager {
         lct.registration_state_changed = registrationStateChanged
         lct.call_state_changed = callStateChanged
         
-        lc = linphone_core_new_with_config(&lct, lpConfig, nil)
+        LinphoneManager.lc = linphone_core_new_with_config(&lct, lpConfig, nil)
         
         // Set ring asset
         let ringbackPath = NSURL(fileURLWithPath: NSBundle.mainBundle().bundlePath).URLByAppendingPathComponent("/ringback.wav").absoluteString
-        linphone_core_set_ringback(lc, ringbackPath)
+        linphone_core_set_ringback(LinphoneManager.lc, ringbackPath)
         
         let localRing = NSURL(fileURLWithPath: NSBundle.mainBundle().bundlePath).URLByAppendingPathComponent("/toy-mono.wav").absoluteString
-        linphone_core_set_ring(lc, localRing)
+        linphone_core_set_ring(LinphoneManager.lc, localRing)
+    }
+    
+    func startLinphone() {
+        let proxyConfig = setIdentify()
+        register(proxyConfig)        
+        setTimer()
+    }
+    
+    @objc func iterate(){
+        linphone_core_iterate(LinphoneManager.lc); /* first iterate initiates registration */
+    }
+    
+    func shutdown(){
+        NSLog("Shutdown..")
+        
+        LinphoneManager.iterateTimer.invalidate()
+        
+        let proxy_cfg = linphone_core_get_default_proxy_config(LinphoneManager.lc); /* get default proxy config*/
+        linphone_proxy_config_edit(proxy_cfg); /*start editing proxy configuration*/
+        linphone_proxy_config_enable_register(proxy_cfg, 0); /*de-activate registration for this proxy config*/
+        linphone_proxy_config_done(proxy_cfg); /*initiate REGISTER with expire = 0*/
+        while(linphone_proxy_config_get_state(proxy_cfg) !=  LinphoneRegistrationCleared){
+            linphone_core_iterate(LinphoneManager.lc); /*to make sure we receive call backs before shutting down*/
+            ms_usleep(50000);
+        }
+        
+        linphone_core_destroy(LinphoneManager.lc);
     }
     
     private func bundleFile(file: NSString) -> NSString{
@@ -94,34 +115,7 @@ class LinphoneManager {
         return documentsPath.stringByAppendingPathComponent(file as String)
     }
     
-    func startLinphone() {
-        idle()
-    }
-    
-    func makeCall(){
-        let calleeAccount = "0702552520"
-        
-        setIdentify()
-        linphone_core_invite(lc, calleeAccount)
-        mainLoop(10)
-        shutdown()
-    }
-    
-    func receiveCall(){
-        let proxyConfig = setIdentify()
-        register(proxyConfig)
-        mainLoop(60)
-        shutdown()
-    }
-    
-    func idle(){
-        let proxyConfig = setIdentify()
-        register(proxyConfig)
-//        mainLoop(100)
-//        shutdown()
-    }
-    
-    func setIdentify() -> COpaquePointer {
+    private func setIdentify() -> COpaquePointer {
         
         // Reference: http://www.linphone.org/docs/liblinphone/group__registration__tutorials.html
         
@@ -146,7 +140,7 @@ class LinphoneManager {
         }
         
         let info=linphone_auth_info_new(linphone_address_get_username(from), nil, password, nil, nil, nil); /*create authentication structure from identity*/
-        linphone_core_add_auth_info(lc, info); /*add authentication info to LinphoneCore*/
+        linphone_core_add_auth_info(LinphoneManager.lc, info); /*add authentication info to LinphoneCore*/
         
         // configure proxy entries
         linphone_proxy_config_set_identity(proxy_cfg, identity); /*set identity with user name and domain*/
@@ -156,41 +150,58 @@ class LinphoneManager {
         
         linphone_proxy_config_set_server_addr(proxy_cfg, server_addr!); /* we assume domain = proxy server address*/
         linphone_proxy_config_enable_register(proxy_cfg, 0); /* activate registration for this proxy config*/
-        linphone_core_add_proxy_config(lc, proxy_cfg); /*add proxy config to linphone core*/
-        linphone_core_set_default_proxy_config(lc, proxy_cfg); /*set to default proxy*/
+        linphone_core_add_proxy_config(LinphoneManager.lc, proxy_cfg); /*add proxy config to linphone core*/
+        linphone_core_set_default_proxy_config(LinphoneManager.lc, proxy_cfg); /*set to default proxy*/
         
         return proxy_cfg
     }
     
-    func register(proxy_cfg: COpaquePointer){
+    private func register(proxy_cfg: COpaquePointer){
         linphone_proxy_config_enable_register(proxy_cfg, 1); /* activate registration for this proxy config*/
     }
     
-    func mainLoop(sec: Int){
-        let time = sec * 100
-        /* main loop for receiving notifications and doing background linphonecore work: */
-        for _ in 1...time{
-            linphone_core_iterate(lc); /* first iterate initiates registration */
-            ms_usleep(10000);
-        }
-    }
-    
-    func shutdown(){
-        NSLog("Shutdown..")
-        
-        let proxy_cfg = linphone_core_get_default_proxy_config(lc); /* get default proxy config*/
-        linphone_proxy_config_edit(proxy_cfg); /*start editing proxy configuration*/
-        linphone_proxy_config_enable_register(proxy_cfg, 0); /*de-activate registration for this proxy config*/
-        linphone_proxy_config_done(proxy_cfg); /*initiate REGISTER with expire = 0*/
-        while(linphone_proxy_config_get_state(proxy_cfg) !=  LinphoneRegistrationCleared){
-            linphone_core_iterate(lc); /*to make sure we receive call backs before shutting down*/
-            ms_usleep(50000);
-        }
-        
-        linphone_core_destroy(lc);
+    private func setTimer(){
+        LinphoneManager.iterateTimer = NSTimer.scheduledTimerWithTimeInterval(
+            0.02, target: self, selector: #selector(iterate), userInfo: nil, repeats: true)
     }
 }
 
 
+//var answerCall: Bool = false
+//if answerCall{
+//    ms_usleep(3 * 1000 * 1000); // Wait 3 seconds to pickup
+//    linphone_core_accept_call(lc, call)
+//}
+//func makeCall(){
+//    let calleeAccount = "0702552520"
+//
+//    setIdentify()
+//    linphone_core_invite(LinphoneManager.lc, calleeAccount)
+//    mainLoop(10)
+//    shutdown()
+//}
+//
+//func receiveCall(){
+//    let proxyConfig = setIdentify()
+//    register(proxyConfig)
+//    mainLoop(60)
+//    shutdown()
+//}
+//
+//func idle(){
+//    let proxyConfig = setIdentify()
+//    register(proxyConfig)
+//    mainLoop(100)
+//    //        shutdown()
+//}
+//func mainLoop(sec: Int){
+//
+//    let time = sec * 100
+//    /* main loop for receiving notifications and doing background linphonecore work: */
+//    for _ in 1...time{
+//        linphone_core_iterate(LinphoneManager.lc); /* first iterate initiates registration */
+//        ms_usleep(10000);
+//    }
+//}
 
         
