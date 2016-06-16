90bfc7950a98db1fe10662739a9d9fd3aca7e502
Cody Liu <liu@codylab.com>
2016-06-16 14:18:35 +0800

    Modify styling

--- GoodWox/CompanyController.swift
+++ GoodWox/CompanyController.swift
@@ -23,8 +23,6 @@ class CompanyController: UITableViewController{
     
         modifyTableStyle()
         downloadCompanyContact()
-        
-        self.tableView.separatorStyle = .None
     }
     
     func downloadCompanyContact(){
